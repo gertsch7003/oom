d1ea665d8e2749ad5d33f1f42ebe98cbb3bb45b1
gertsch7003 <gerhardsabeditsch@gmx.net>
2016-02-20 17:59:44 +0100

    Task2_homework_done
    Task2_homework_done
    —class House

---
+++ notes_in_lesson/note_lesson_2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+
+
+
+
+namespace note_lesson_2
+{
+	class Buch
+	{
+		//public = jeder darf in dieses Feld preis reinschreiben
+		//public double preis;
+
+		// man sieht nur in aktueller klasse
+		//private double preis;
+
+		private string titel;
+		private decimal preis;
+
+		public Buch(string derTitel, decimal derPreis)
+		{
+
+			titel = derTitel;
+			SetPreis(derPreis);
+
+
+
+		}
+
+
+
+		public string ISBN { get; private set;}
+
+
+
+
+		public string Titel
+		{
+
+			get {
+				return titel;
+			}
+			set{
+				if (string.IsNullOrEmpty (value) || value.Length < 3) {
+					throw new Exception ("muss groesser 3 sein");
+				}
+			}
+		
+
+
+
+		public decimal GetPreis()
+		{
+
+			return preis;
+
+		}
+
+
+		public void SetPreis(decimal neuerPreis)
+		{
+
+			if (neuerPreis < 0) throw new Exception ("Preis muss positiv sein");
+			preis = neuerPreis;
+
+		}
+
+
+
+
+
+	}
+
+
+
+	class Programm
+	{
+
+
+
+
+		static void Main(string[] args)
+		{
+
+
+			var a = new Buch (9);
+
+			a.SetPreis (100);
+
+
+
+		
+
+			Console.WriteLine (a.GetPreis());
+
+
+		}
+
+
+
+
+
+
+	}
+
+
+
+
+}
---
+++ tasks/Task2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Task2
+{
+
+
+	class House
+	{
+
+		public int m_house_nr;
+		public string m_street;
+
+
+		private string m_familie_name;
+
+
+		//Constructor
+		public House(int house_nr, string street, string familie_name)
+		{
+
+		
+			m_house_nr = house_nr;
+			m_street = street;
+			m_familie_name = setFamilieName(familie_name);
+
+		}
+
+
+		//Methode for Set m_familie_name
+		public string setFamilieName(string Name)
+		{
+			m_familie_name = Name;
+
+
+			return m_familie_name;
+		}
+
+
+		public string getFamilieName()
+		{
+			return m_familie_name;
+		}
+
+
+
+	}
+
+
+
+	class Programm
+	{
+		public static void Main (string[] args)
+		{
+
+			House myhouse = new House(11,"Hauptstraße","Hummer");
+
+
+			Console.WriteLine ("\n\nHAUS_NR: {0}  STRASSE: {1}  Fam.Name: {2}\n\n",myhouse.m_house_nr,myhouse.m_street,myhouse.getFamilieName());
+		
+			
+			Console.WriteLine ("Fam.Name geaendert auf: {0}",myhouse.setFamilieName("Baglawatsch"));
+
+
+			Console.WriteLine ("\n\nHAUS_NR: {0}  STRASSE: {1}  Fam.Name: {2}\n\n",myhouse.m_house_nr,myhouse.m_street,myhouse.getFamilieName());
+		
+		}
+	}
+
+
+
+
+
+
+}
