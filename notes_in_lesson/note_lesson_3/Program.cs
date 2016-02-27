c3e3f646bdecfbdcebc1adcf2f1484ca2623141b
gertsch7003 <gerhardsabeditsch@gmx.net>
2016-02-27 15:34:05 +0100

    Task3_commit

---
+++ notes_in_lesson/note_lesson_3/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+
+
+
+
+namespace note_lesson_3
+{
+
+
+
+	class Person
+	{
+
+		public string Name { get; }
+
+		public string Email { get; }
+
+
+		// um das zu machen, muss die classe auch von abstract sein
+		public abstract void DoSomething();
+
+
+		//public virtuel void --> Kindklassen dürfen diese Methode ersetzen !!
+		public virtual void DoSomethingCool()
+		{
+
+			Console.WriteLine ("COOOLE SACHE");
+
+		}
+
+
+
+	}
+
+
+	class Kunde : Person
+	{
+
+		public int BonusPunkte { get; }
+
+		// override braucht man um dann die Eltern Methode wirklich ersetzt
+		public override void DoSomethingCool()
+		{
+
+			Console.WriteLine ("COOOLE SACHE -- different");
+
+		}
+
+	}
+
+
+
+
+	class Angestellter : Person
+	{
+
+		public int SVN { get; }
+
+
+	}
+
+
+
+
+
+
+	class Programm
+	{
+
+
+//		public string Name
+//		{
+//
+//			get { return "Sabeditsch";}
+//
+//		}
+
+		public string Name => "Sabeditsch";
+
+
+
+		public static double Sqr2(double x) => x * x;
+
+
+
+
+		static double Sqr(double x)
+		{
+
+			return x * x;
+
+		}
+
+
+		static int[] Map(int[] data, Func<int, int> f )
+		{
+
+			var result = new int[data.Length];
+
+			
+			for (var i = 0; i < data.Length; i++) {
+				result [i] = f (data [i]);
+			}
+
+
+			return result;
+
+		}
+
+
+		static T[] Map2<T>(int[] data, Func<int, int> f )
+		{
+
+			var result = new T[data.Length];
+
+
+			for (var i = 0; i < data.Length; i++) {
+				result [i] = f (data [i]);
+			}
+
+
+			return result;
+
+		}
+
+
+
+		public static void Main (string[] args)
+		{
+
+			var xs = new[] {1,2,3,4,5,6,7,8,9};
+
+			var xsT = new[] {1.0,2,3,4,5,6,7,8,9};
+
+
+			var ys = Map (xs, Sqr);
+
+			var zs = Map2 (xsT,xx => xx + 1);
+
+
+
+			var x = Math.Sin (Math.PI / 2);
+
+
+
+			//Action  -- Wenn die Funktion keinen Rückgabewert besitzt
+			Action<string> log = Console.WriteLine;
+
+			// <Agumente uebergeben,Return Type>
+			Func<double, double> f = Math.Sin;
+
+
+			log ("HALLO");
+
+
+
+
+
+
+
+			Console.WriteLine (x);
+		}
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+}
---
+++ tasks/Task3/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+
+namespace Task3
+{
+
+
+
+	public interface ICity
+	{
+
+		string setFamilieName(string Name);
+
+		string getFamilieName();
+
+
+	}
+
+
+
+	class House :ICity
+	{
+
+		//Fields public
+		public int m_house_nr;
+		public string m_street;
+
+		//Fields privat
+		private string m_familie_name;
+
+
+
+
+		//Constructor
+		public House(int house_nr, string street, string familie_name)
+		{
+
+
+			m_house_nr = house_nr;
+			m_street = street;
+			m_familie_name = setFamilieName(familie_name);
+
+		}
+
+
+		//Methode for Set m_familie_name
+		public string setFamilieName(string Name)
+		{
+			m_familie_name = Name;
+
+
+			return m_familie_name;
+		}
+
+
+
+		//Methode for get Familie Name
+		public string getFamilieName()
+		{
+			return m_familie_name;
+		}
+			
+	}
+
+
+
+
+	class Adds : ICity
+	{
+
+		//Fields public
+		public int m_house_nr;
+		public string m_street;
+
+		//Fields privat
+		private string m_familie_name;
+		private string[] m_adds;
+	
+
+
+		//Constructor
+		public Adds(int house_nr, string street, string familie_name, string new_add_name)
+		{
+
+
+			m_house_nr = house_nr;
+			m_street = street;
+			m_familie_name = setFamilieName(familie_name);
+			set_adds(new_add_name);
+		}
+			
+
+		//Methode for Set m_familie_name
+		public string setFamilieName(string Name)
+		{
+			m_familie_name = Name;
+
+
+			return m_familie_name;
+		}
+
+
+
+		//Methode for get Familie Name
+		public string getFamilieName()
+		{
+			return m_familie_name;
+		}
+
+
+		public void set_adds(string add_name)
+		{
+
+			m_adds = new string[1];
+
+			m_adds[0] = add_name;
+
+		}
+
+
+		public void get_adds()
+		{
+
+			for(int i=0;i< m_adds.Length;i++)
+			{
+				Console.WriteLine("Add Abo {0} is {1}",i,m_adds[i]);
+			}
+
+
+		}
+
+
+
+
+	}
+
+
+
+
+
+
+	class Programm
+	{
+		public static void Main (string[] args)
+		{
+
+			House myhouse = new House(11,"Hauptstraße","Hummer");
+
+
+			Console.WriteLine ("\n\nHAUS_NR: {0}  STRASSE: {1}  Fam.Name: {2}\n\n",myhouse.m_house_nr,myhouse.m_street,myhouse.getFamilieName());
+
+
+			Console.WriteLine ("Fam.Name geaendert auf: {0}",myhouse.setFamilieName("Baglawatsch"));
+
+
+			Console.WriteLine ("\n\nHAUS_NR: {0}  STRASSE: {1}  Fam.Name: {2}\n\n",myhouse.m_house_nr,myhouse.m_street,myhouse.getFamilieName());
+
+
+
+
+			Adds add1 = new Adds (12, "Uferstraße", "Neumaier", "Obbi");
+
+			add1.get_adds();
+
+
+
+		}
+	}
+
+
+
+
+
+
+
+}
