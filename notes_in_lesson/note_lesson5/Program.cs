e1ca15ae91dc15fa80a6ce232540b9f803f28c1d
gertsch7003 <gerhardsabeditsch@gmx.net>
2016-03-28 17:28:50 +0200

    notes_in_lesson5

---
+++ notes_in_lesson/note_lesson5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace note_lesson5
+{
+
+
+
+
+
+
+
+
+
+
+	class MainClass
+	{
+
+
+
+
+		public static void Main (string[] args)
+		{
+
+
+			var xs= new List<int> {1,2,3,4,5,6,7,8,9};
+
+
+
+			foreach (var x in xs)
+			{
+				Console.WriteLine (x);
+			}
+
+			//= hinter den kolissen:
+			var e = xs.GetEnumerator ();
+
+
+			while (e.MoveNext ()) {
+			
+			
+				Console.WriteLine (e.Current);
+			
+			
+			}
+
+
+
+
+
+
+
+
+
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
